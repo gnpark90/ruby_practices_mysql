15f8ed39ac08c2c856f4c9564d42744e9aca68b9
k.park <gnpark90@gmail.com>
2021-01-31 15:47:47 +0900

    Created all files related with practices rails (sign-up)

--- app/models/test_user.rb
+++ app/models/test_user.rb
@@ -6,9 +6,7 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
-  .
-  .
-  .
+  
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
